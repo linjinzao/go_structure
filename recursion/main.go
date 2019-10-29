77eabe65f3854ae11fb06782f55c6e0aebb2dc78
linjinzao <526649566@qq.com>
2019-10-29 23:17:30 +0800

    递归翻转数组

--- recursion/main.go
+++ recursion/main.go
@@ -19,10 +19,27 @@ func sumArr(arr [5] int ,n int) int {
 	return sumArr(arr,n-1) + arr[n]
 }
 
+//反转数组
+func reverse(arr *[5] int,lo,hi int) {
+	if(lo < hi){
+		var temp int 
+		temp = (*arr)[lo]
+		(*arr)[lo] = (*arr)[hi]
+		(*arr)[hi] = temp
+		reverse(&(*arr),lo+1 ,hi-1)
+	}
+}
+
 func main(){
 	 var arr = [5] int {10,20,30,4,1}
 	 sum1 := sumI(arr,5)
-	fmt.Printf("sum1 = %g",sum1)
+	fmt.Printf("sum1 = %d",sum1)
 	sum2 := sumArr(arr,4)
-	fmt.Printf("sun2 = %g",sum2)
+	fmt.Printf("sun2 = %d",sum2)
+
+	reverse(&arr,0,4)
+
+	for key,val := range arr{
+		fmt.Printf("index %d is %d",key,val)
+	}
 }
